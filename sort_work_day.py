bd01e231cfc72efd412becf85bb503954abf7434
Fad5 <andreyka.anokhin@mail.ru>
2023-10-20 13:27:01 +0300

    Function restart changes

--- sort_work_day.py
+++ sort_work_day.py
@@ -3,10 +3,10 @@ import pandas as pd
 import threading
 
 
-def create_cvs_file():
+def create_cvs_file() -> None:
     """
     Функция забирает онлайн таблицу google, проходится pandas и создает csv с данными
-    :return:
+    :return: None
     """
     df = pd.read_csv(
         'https://docs.google.com/spreadsheets/d/1vZc0LfpNmLZKnvn4EiEeD-HTZE5UJ2wBZX5fOF6FtBk/export?format=csv')
@@ -16,15 +16,4 @@ def create_cvs_file():
          'аккаунт']]  # Выберем из датафрейма 2 столбца и сохраним в новый датафрейм
     new_df.to_csv('data_base.csv', index=False)  # Экспорт в CSV файл
     print('Succusfull', datetime.datetime.now())
-    data_append = f'Succusfull {str(datetime.datetime.now())}'
-    threading.Timer(1800, create_cvs_file).start()
-
-
-def create_cvs__file():
-    """
-    Функция забирает онлайн таблицу google, проходится pandas и создает csv с данными
-    :return:
-    """
-    df = pd.read_csv(
-        'https://docs.google.com/spreadsheets/d/1ciHzc4lZ9tMaKGUF4xOTm_QsY6ostSCGegyIDS-n4ds/export?format=csv')
-    print(df.iloc[12])
+    threading.Timer(3600, create_cvs_file).start()
